bf3a721c6bb2b973faa0dc392a4b023518749d0f
JayaSangwani <jayasangwani466@gmail.com>
2024-01-06 23:21:51 +0530

    LAB014

---
+++ src/LAB014.java
@@ -0,0 +1,18 @@
+package src;
+import java.util.Scanner;
+
+public class LAB014 {
+    public static void main(String[] args) {
+        //swapping
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of a");
+        int a = sc.nextInt();
+        System.out.println("Enter value of b");
+        int b = sc.nextInt();
+
+        int temp = a;
+        a=b;
+        b=temp;
+        System.out.println("a= "+a+"   "+"b= "+b);
+    }
+}
