c85df998cc1e2b69673eaf314cb67f0fa748759f
JayaSangwani <jayasangwani466@gmail.com>
2024-03-02 23:17:13 +0530

    encap02/03

---
+++ src/AllPrograms/LAB018.java
@@ -0,0 +1,14 @@
+package src.AllPrograms;
+
+
+public class LAB018 {
+    {
+        System.out.println("I am jaya");
+    }
+}
+
+class Hello{
+    {
+        System.out.println("Testing");
+    }
+}
